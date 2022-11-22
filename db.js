52cb466cf074f53481a9a88266f531f0bc12a07f
abhidp <abhi.daspatnik@optus.com.au>
2022-11-22 23:28:17 +1100

    working

--- db.js
+++ db.js
@@ -1,4 +1,9 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const client = new DynamoDBClient({});
+const client = new DynamoDBClient({
+  region: 'ap-southeast-2',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  endpoint: 'http://localhost:8000',
+});
 
 module.exports = client;
